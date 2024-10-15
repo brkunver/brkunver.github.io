3079a05e57f1a5eadb2752cfe674f056f6e37a3e
Burakhan Ünver <43881727+brkunver@users.noreply.github.com>
2024-10-15 13:22:52 +0300

    Init wolf.ts

--- src/wolf.ts
+++ src/wolf.ts
@@ -1,15 +1,44 @@
-import p5 from "p5";
+import p5 from "p5"
 
-class Wolf{
+class Wolf {
+  p5: p5
+  position: p5.Vector
+  down_sprites_paths: Array<string> = [
+    "assets/wolf/down/d0.png",
+    "assets/wolf/down/d1.png",
+    "assets/wolf/down/d2.png",
+    "assets/wolf/down/d3.png",
+    "assets/wolf/down/d4.png",
+    "assets/wolf/down/d5.png",
+  ]
+  direction: "u" | "d" | "l" | "r" | "ul" | "ur" | "dl" | "dr" | "idle" = "idle"
+  isMoving: boolean = false
 
-  position : p5.Vector
-  
-  constructor(posiition : p5.Vector){
-    this.position = posiition
+  // Animasyon frame index ve yüklenmiş sprite'lar
+  spriteImages: Array<p5.Image> = []
+  frameIndex: number = 0
+  frameDelay: number = 5 // Her 5 frame'de bir sprite değişecek
+  frameCounter: number = 0
+
+  constructor(p5: p5, position: p5.Vector) {
+    this.p5 = p5
+    this.position = position
+
+    this.down_sprites_paths.forEach((path) => {
+      this.spriteImages.push(this.p5.loadImage(path))
+    })
   }
 
-  draw(p : p5){
-    p.ellipse(this.position.x, this.position.y, 20, 20)
-  }
+  draw() {
+    if (this.frameCounter % this.frameDelay === 0) {
+      // Sonraki sprite'a geç
+      this.frameIndex = (this.frameIndex + 1) % this.spriteImages.length
+    }
+    this.frameCounter++
 
+    // Mevcut sprite'ı çiz
+    this.p5.image(this.spriteImages[this.frameIndex], this.position.x, this.position.y)
+  }
 }
+
+export default Wolf
