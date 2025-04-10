1c6be7c0f373b9cd65bbc2672bda62f4f097daa4
brkunver <burakhanunver@gmail.com>
2025-04-10 18:25:31 +0300

    refactor for cloudflare switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://brkunver.github.io",
+  site: "https://kunver.dev",
   integrations: [sitemap()],
   prefetch: true,
   vite: {
