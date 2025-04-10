3bd1953bd88f1bda9e35b74af267d3c0708b19ad
brkunver <burakhanunver@gmail.com>
2025-04-10 18:15:31 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config"
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://brkunver.github.io",
-  integrations: [],
-
+  integrations: [sitemap()],
+  prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
